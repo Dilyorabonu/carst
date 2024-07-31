ae67ef9183a254a025f8422bc15abd1c77b9b419
dilyorabonu <dilyorabonu@gmail.com>
2024-07-31 09:29:54 +0500

    fixed

--- src/pages/Rating.jsx
+++ src/pages/Rating.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+// src/pages/Rating.jsx
+import React from "react";
+import { useLocation } from "react-router-dom";
+import BarChart from "../components/Barchart";
 
-function Rating() {
+const Rating = () => {
+  const location = useLocation();
+  const { data, categories } = location.state || { data: [], categories: [] };
+
   return (
-    <div>Rating</div>
-  )
-}
+    <div className="container mx-auto p-6">
+      <h2 className="text-2xl font-bold mb-4">Model Ratings</h2>
+      <BarChart data={data} categories={categories} />
+    </div>
+  );
+};
 
-export default Rating
+export default Rating;
